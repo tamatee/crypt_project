f555d92c460da2f1e83b3f5871de1aa4b0da558b
tamatee <teeracharoenkitisap@gmail.com>
2025-03-23 14:59:25 +0700

    [main -> main] init project

---
+++ crypto.h
@@ -0,0 +1,2 @@
+int ft_gcd(int a, int b);
+int ft_inversegcd(int a, int b, int c);
---
+++ ft_gcd.c
@@ -0,0 +1,7 @@
+int ft_gcd(int a, int b) {
+    while (b != 0){
+        a = b;
+        b = a % b;
+    }
+    return a;
+}
