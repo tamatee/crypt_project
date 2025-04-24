5aad54a4a0b85fb2934fbac9b05d8abe612b6b21
tamatee <teeracharoenkitisap@gmail.com>
2025-04-24 13:06:10 +0700

    [main -> main] wipe code and correctness

--- T/send_message.py
+++ T/send_message.py
@@ -1,10 +1,9 @@
-from elgamal import ElgamalEncrypt
-from utils import load_key
+from elgamal import ElgamalEncrypt, load_key
 import os
 
 print("ENCRYPTION")
 public_key = load_key('recv_keys/public_key.txt')
 input_filename = input("file_name: ")
 os.makedirs('encrypted', exist_ok=True)
-ciphertext = ElgamalEncrypt(input_filename, public_key, output_path='encrypted/from_ciphertext.dat')
+ciphertext = ElgamalEncrypt(input_filename, public_key, output_path='encrypted/image_cipher.dat')
 print("Ciphertext (A,B):", ciphertext)
